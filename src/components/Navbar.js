45bed5825dfb795de7eea1e1cc55c7e1555e8279
Gildas Niyigena <gildniy05@gmail.com>
2019-12-24 08:59:54 +0200

    chore(tests): setup tests for client - setup jest library - install enzyme and react-testing-library - add command required for testing in the README [Finishes#169257336]

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,9 +4,9 @@ import { Link } from 'react-router-dom';
 export default function Navbar() {
 	return (
 		<div data-testid='navbar'>
-			<Link to='/'>Home page</Link>
-			<Link to='/login'>Login</Link>
-			<Link to='/Signup'>signup</Link>
+			<Link to='/'>Home</Link>
+			<Link to='/login'>login</Link>
+			<Link to='/register'>register</Link>
 			<Link to='/profile'>profile</Link>
 		</div>
 	);
